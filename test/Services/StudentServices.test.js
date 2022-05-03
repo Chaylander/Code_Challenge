a2359df4c0605691a44cec86a695fc6d9220f25b
RobertoJáuregui <robotrjj@hotmail.com>
2022-05-03 14:53:58 -0500

    Clase para sacar los emails de los estudiantes con certificacion, y su respectiva prueba de unidad

--- test/Services/StudentServices.test.js
+++ test/Services/StudentServices.test.js
@@ -4,7 +4,15 @@ const StudentServices = require("./../../../code_challenge/lib/Services/StudentS
 describe("Test para la clase StudentServices", () => {
     test("Requerimiento 1: Retornar el arreglo con los estudiantes", () => {
         const students = Reader.readJsonFile("visualpartners.json");
-        const AllStudents = StudentServices.GetAllStudents(students);
+        const AllStudents = StudentServices.getAllStudents(students);
         expect(AllStudents).toEqual(students);
     });
+    test("Requerimiento 2: consultar los mails de los estudiantes que tengan certificacion", () =>{
+        const students = Reader.readJsonFile("visualpartners.json");
+        const StudentsMailWithCertification = StudentServices.getStudentsMailWithCertification(students, true);
+        expect(StudentsMailWithCertification.length).toBe(29);
+    });
+
+
+
 });
