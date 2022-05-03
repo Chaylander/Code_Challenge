5fec444225a7833818f776ef0d5b41f37d3f4199
RobertoJáuregui <robotrjj@hotmail.com>
2022-05-03 16:10:08 -0500

    Agrego funcion para obtener los estudiantes con un credito mayor>x en student controller para conectar a la API con su respectiva prueba de unidad"

--- test/Controllers/StudentController.test.js
+++ test/Controllers/StudentController.test.js
@@ -8,7 +8,10 @@ describe("Test para la clase StudentController, la cual sirve para conectar a la
     test("Requerimiento 2: Retornar correo de los estudiantes con certificación", () => {
         const getEmailsWithCertification = StudentController.getStudentsMailWithCertification(true);
         expect(getEmailsWithCertification[0]).toBe("Todd@visualpartnership.xyz");
-
+    });
+    test("Requerimiento 3: Estudiantes con credito mayor a 500", () => {
+        const getStudentsWithMoreThan500Credits = StudentController.getStudentsWithCredits(500);
+        expect(getStudentsWithMoreThan500Credits[0].name).toBe("Warren");
     });
 
 });
