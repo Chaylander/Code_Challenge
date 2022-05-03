a1387adc547a6cbaf129cf491802419ab48df27e
RobertoJáuregui <robotrjj@hotmail.com>
2022-05-03 16:02:18 -0500

    Agrego funcion en studentcontroller para obtener los correos de los estudiantes con certificacion

--- test/Controllers/StudentController.test.js
+++ test/Controllers/StudentController.test.js
@@ -5,5 +5,10 @@ describe("Test para la clase StudentController, la cual sirve para conectar a la
         const AllStudents = StudentController.getAllStudents();
         expect(AllStudents.length).toBe(51);
     });
+    test("Requerimiento 2: Retornar correo de los estudiantes con certificación", () => {
+        const getEmailsWithCertification = StudentController.getStudentsMailWithCertification(true);
+        expect(getEmailsWithCertification[0]).toBe("Todd@visualpartnership.xyz");
+
+    });
 
 });
